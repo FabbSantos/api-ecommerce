e3b64f8134df1bb9e9717e4566eba6218e4abb73
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2022-11-10 11:08:25 -0300

    consegui obter uma linha do banco e mostrar como json.

--- pages/api/inserirProd.ts
+++ pages/api/inserirProd.ts
@@ -1,13 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import PocketBase from 'pocketbase'
 
-type Data = {
-    id: String
-}
+const client = new PocketBase('http://127.0.0.1:8090/')
+
+const authAdmin =  await client.admins.authViaEmail('fabriciobs2000@gmail.com', 'Fabriciobs@1');
+const record = await client.records.getOne('products', 'ronftaf3b4ylrxm', {
+    expand: 'some_relation'
+});
 
 
-export default function test(req: NextApiRequest, res: NextApiResponse<Data>) {
-    if (req.body.name) {
-        const response = (req.body.name)
-        res.status(200).json(response)
-    }
+
+// type Data = {
+//     id: Record
+// }
+
+
+export default function test(req: NextApiRequest, res: NextApiResponse<any>) {
+    // if (req.body.name) {
+    //     const response = (req.body.name)
+    //     res.status(200).json(response)
+    // }    
+
+    res.status(200).json(record)
 }
